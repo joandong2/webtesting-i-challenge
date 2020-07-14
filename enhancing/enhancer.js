d143ebfa8105552524530fad8d76b301a1f0540c
John Oblenda <joandong2@gmail.com>
2020-07-14 17:26:10 -0400

    added tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,47 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+    succeed,
+    fail,
+    repair,
+    get,
 };
 
 function succeed(item) {
-  return { ...item };
+    if (item.enhancement < 0) {
+        item.enhancement = 0;
+    } else if (typeof item.enhancement !== "number") {
+        throw new Error("enhancement is invalid");
+    } else if (item.enhancement < 20) {
+        item.enhancement++;
+    }
+    return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+    if (typeof item.enhancement !== "number") {
+        throw new Error("enhancement is invalid");
+    }
+
+    if (item.enhancement >= 15) {
+        item.durability -= 10;
+
+        if (item.enhancement > 16) {
+            item.enhancement--;
+        }
+    } else {
+        item.durability -= 5;
+    }
+
+    return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+    if (typeof item.durability !== "number") {
+        throw new Error("durability is invalid");
+    }
+
+    return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+    return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,50 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const items = [
+    { name: "Broad Sword", durability: 55, enhancement: 16 },
+    { name: "Battle Fury", durability: -3, enhancement: -2 },
+    { name: "Bracer", durability: "fasd", enhancement: "abc" },
+];
+
+test("successfully enhances an item", () => {
+    expect(enhancer.succeed(items[0])).toEqual({
+        name: "Broad Sword",
+        durability: 55,
+        enhancement: 17,
+    });
+    expect(enhancer.succeed(items[1])).toEqual({
+        name: "Battle Fury",
+        durability: -3,
+        enhancement: 0,
+    });
+    expect(() => enhancer.succeed(items[2])).toThrow();
+});
+
+test("enahancement failed", () => {
+    expect(enhancer.fail(items[0])).toEqual({
+        name: "Broad Sword",
+        durability: 45,
+        enhancement: 16,
+    });
+    expect(enhancer.fail(items[1])).toEqual({
+        name: "Battle Fury",
+        durability: -8,
+        enhancement: 0,
+    });
+    expect(() => enhancer.fail(items[2])).toThrow();
+});
+
+test("repair an item", () => {
+    expect(enhancer.repair(items[0])).toEqual({
+        name: "Broad Sword",
+        durability: 100,
+        enhancement: 16,
+    });
+    expect(enhancer.repair(items[1])).toEqual({
+        name: "Battle Fury",
+        durability: 100,
+        enhancement: 0,
+    });
+    expect(() => enhancer.repair(item[2])).toThrow();
+});
